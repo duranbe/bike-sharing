b46ec0fc04cd4fbd064031ebe185610748553292
Benoît Durand <benoit.durand.mail@gmail.com>
2021-12-26 13:24:15 +0100

    add pymongo and fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.8-alpine
+FROM python:3.8
+
 
 RUN pip install --upgrade pip 
 
--- app/main.py
+++ app/main.py
@@ -2,16 +2,27 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import pymongo
+import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="./app/templates")
 
+db_user = os.environ['db_user']
+db_name = os.environ['db_name']
+db_password = os.environ['db_password']
+db_cluster = os.environ['db_cluster']
+
+client = pymongo.MongoClient(f"mongodb+srv://{db_user}:{db_password}@{db_cluster}/{db_name}?retryWrites=true&w=majority")
+
+db = client[db_name]
+
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request):
     """
     Main
     """
-
+    
     return templates.TemplateResponse("index.html", {"request": request})
